90ce2858071718d8793e5436ca2586e1ca5e1618
Віталій <vitaliy.shandar@gmail.com>
2024-01-10 21:08:01 +0200

    Commit

---
+++ src/main/java/org/example/service/DatabaseQueryService.java
@@ -0,0 +1,149 @@
+package org.example.service;
+
+import org.example.db.ReadSqlFile;
+import org.example.db.Database;
+import org.example.model.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DatabaseQueryService {
+
+    public List<FindLongestProject> findLongestProject() {
+        List<FindLongestProject> findLongestProjectList = new ArrayList<>();
+
+        String urlFile = "sql/find_longest_project.sql";
+        ReadSqlFile readSqlFile = new ReadSqlFile();
+        String requestSql = readSqlFile.read(urlFile);
+
+        try (Connection connection = Database.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(requestSql);
+
+            while (resultSet.next()) {
+                FindLongestProject findLongestProject = new FindLongestProject();
+                findLongestProject.setProjectId(resultSet.getInt("PROJECT_ID"));
+                findLongestProject.setMouthCount(resultSet.getInt("MONTHS_COUNT"));
+                findLongestProjectList.add(findLongestProject);
+            }
+
+            statement.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return findLongestProjectList;
+    }
+
+    public List<FindMaxProjectsClient> findMaxProjectsClients() {
+        List<FindMaxProjectsClient> findMaxProjectsClientList = new ArrayList<>();
+
+        String urlFile = "sql/find_max_projects_client.sql";
+        ReadSqlFile readSqlFile = new ReadSqlFile();
+        String requestSql = readSqlFile.read(urlFile);
+
+        try (Connection connection = Database.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(requestSql);
+
+            while (resultSet.next()) {
+                FindMaxProjectsClient findMaxProjectsClient = new FindMaxProjectsClient();
+                findMaxProjectsClient.setName(resultSet.getString("NAME"));
+                findMaxProjectsClient.setProjectCount(resultSet.getInt("PROJECT_COUNT"));
+                findMaxProjectsClientList.add(findMaxProjectsClient);
+            }
+
+            statement.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return findMaxProjectsClientList;
+    }
+
+    public List<FindMaxSalaryWorker> findMaxSalaryWorker() {
+        List<FindMaxSalaryWorker> findMaxSalaryWorkerList = new ArrayList<>();
+
+        String urlFile = "sql/find_max_salary_worker.sql";
+        ReadSqlFile readSqlFile = new ReadSqlFile();
+        String requestSql = readSqlFile.read(urlFile);
+
+        try (Connection connection = Database.getConnection()) {
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(requestSql);
+
+            while (resultSet.next()) {
+                FindMaxSalaryWorker findMaxSalaryWorker = new FindMaxSalaryWorker();
+                findMaxSalaryWorker.setName(resultSet.getString("NAME"));
+                findMaxSalaryWorker.setSalary(resultSet.getInt("SALARY"));
+                findMaxSalaryWorkerList.add(findMaxSalaryWorker);
+            }
+
+            statement.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return findMaxSalaryWorkerList;
+    }
+
+    public List<FindYoungestEldestWorkers> findYoungestEldestWorkers() {
+        List<FindYoungestEldestWorkers> findYoungestEldestWorkersList = new ArrayList<>();
+
+        String urlFile = "sql/find_youngest_eldest_workers.sql";
+        ReadSqlFile readSqlFile = new ReadSqlFile();
+        String requestSql = readSqlFile.read(urlFile);
+
+        try (Connection connection = Database.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(requestSql);
+
+            while (resultSet.next()) {
+                FindYoungestEldestWorkers findYoungestEldestWorkers = new FindYoungestEldestWorkers();
+                findYoungestEldestWorkers.setType(resultSet.getString("TYPE"));
+                findYoungestEldestWorkers.setName(resultSet.getString("NAME"));
+                findYoungestEldestWorkers.setBirthday(resultSet.getDate("BIRTHDAY"));
+                findYoungestEldestWorkersList.add(findYoungestEldestWorkers);
+            }
+
+            statement.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return findYoungestEldestWorkersList;
+    }
+    public List<PrintProjectPrices> printProjectPrices() {
+        List<PrintProjectPrices> printProjectPricesList = new ArrayList<>();
+
+        String urlFile = "sql/print_project_prices.sql";
+        ReadSqlFile readSqlFile = new ReadSqlFile();
+        String requestSql = readSqlFile.read(urlFile);
+
+        try(Connection connection = Database.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(requestSql);
+
+            while (resultSet.next()) {
+                PrintProjectPrices printProjectPrices = new PrintProjectPrices();
+
+                printProjectPrices.setProjectId(resultSet.getInt("PROJECT_ID"));
+                printProjectPrices.setPrice(resultSet.getInt("PRICE"));
+                printProjectPricesList.add(printProjectPrices);
+            }
+
+            statement.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return printProjectPricesList;
+    }
+}
